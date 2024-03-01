509e2fc69c0c4b3c3fc15073705ccebab819d306
hero890 <159156837+hero890@users.noreply.github.com>
2024-03-01 15:58:47 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -21,7 +21,7 @@ async def start_message(c,m):
     await db.is_user_exist(m.from_user.id)
     await db.add_user(m.from_user.id, m.from_user.first_name)
     await c.send_message(LOG_CHANNEL, LOG_TEXT.format(m.from_user.id, m.from_user.mention))
-    await m.reply_photo(f"https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg",
+    await m.reply_photo(f"https://graph.org/file/8f2690b44cc5ecf49e9a9.jpg",
         caption="**ʜɪ** 👋\n\n**ɪ ᴀᴍ ᴀ ᴄʜᴀᴛɢᴘᴛ ʙᴏᴛ**\n\n⭕ **ᴘᴏᴡᴇʀᴇᴅ ʙʏ :-** **[HEro](https://t.me/hero_botss)**",
         reply_markup=InlineKeyboardMarkup(
                 [
