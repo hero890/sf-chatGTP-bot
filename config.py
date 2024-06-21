baf3458bde8d603f0102b28c685b378a4af9cce9
Tech HEro <159156837+hero890@users.noreply.github.com>
2024-06-21 15:49:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,10 +12,10 @@ def is_enabled(value, default):
 
 API_ID = int(environ.get("API_ID", "22672907"))
 API_HASH = environ.get("API_HASH", "0ff15ae2153bd8e03b48cb293010bc6a")
-BOT_TOKEN = environ.get("BOT_TOKEN", "6635203051:AAHyR3V1zQ2RTYZsfE6kc9C4s5LvLx71Br0")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7087741098:AAFhQ6yKXdScmNcKPlutvHL1sHhUKdb5dws")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002022119783"))
 ADMINS = int(environ.get("ADMINS", "6287591671"))
 DB_URI = environ.get("DB_URI", "mongodb+srv://rani828719:sVyRWZOrUzIWNfHp@cluster0.zodktob.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = environ.get("DB_NAME", "chatgptHErobot")
-OPENAI_API = environ.get("OPENAI_API", "")
+OPENAI_API = environ.get("OPENAI_API", "sk-proj-DhpiKsXCHE09EWYeMkA4T3BlbkFJf46borWbVw5v7dYv3Dkz")
 AI = is_enabled((environ.get("AI","True")), False)
